40b18cfb73f01df069e70d9c9013c15eceafd987
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2024-07-13 12:43:36 -0300

    fix: put a guard on  CREDENTIALS_FILE_PATH since the value doesn't render correctly if use interpolation.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,14 +4,17 @@ set -euo pipefail
 
 
 if [[ -n "${CREATE_AWS_CREDENTIALS_FILE:-}" ]]; then
-  LOCATION="${CREDENTIALS_FILE_PATH:-${HOME}/.aws}}"
-  mkdir -p "${LOCATION}"
+  if [[ -n "${CREDENTIALS_FILE_PATH}" ]]; then
+    CREDENTIALS_FILE_PATH=${HOME}/.aws
+  fi
 
-  cat > "${LOCATION}" <<- EOF
+  mkdir -p "${CREDENTIALS_FILE_PATH%/*}"
+
+  cat > "${CREDENTIALS_FILE_PATH:-${HOME}/.aws/credentials}" <<- EOF
     [default]
     credential_process = aws_signing_helper credential-process --certificate ${CERTIFICATE_FILE_PATH?Please set the CERTIFICATE_FILE_PATH variable.} --private-key ${CERTIFICATE_KEY_PATH?Please set the CERTIFICATE_KEY_PATH variable.} --trust-anchor-arn ${TRUST_ANCHOR_ARN?Please set the TRUST_ANCHOR_ARN variable.} --profile-arn ${PROFILE_ARN?Please set the PROFILE_ARN variable.} --role-arn  ${ROLE_ARN?Please set the ROLE variable.}
 EOF
-  echo "Credential file created at ${LOCATION}"
+  echo "Credential file created at ${CREDENTIALS_FILE_PATH}"
 fi
 
 exec /usr/bin/aws_signing_helper "${@}"
