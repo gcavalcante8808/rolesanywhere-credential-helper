28caf513dfe219aea641e0cd7648b79d82550b64
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2024-07-23 15:53:38 -0300

    chore: initial implementation.

---
+++ wrapper/long_running.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+import boto3
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
+
+session = boto3.Session(
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    aws_session_token=AWS_SESSION_TOKEN
+)
+
+while True:
+    s3 = session.client('s3')
+    print(s3.list_buckets())
+    time.sleep(600)
--- wrapper/wrapper.py
+++ wrapper/wrapper.py
@@ -1,21 +1,54 @@
-#!/usr/bin/env python3
-
-## A very simple script copied from https://github.com/hashicorp/vault/issues/12568 that help in cases where
-## the target binary doesn't have support to use local profiles.
-
-
+import multiprocessing
 import os
+import subprocess
 import sys
+import time
 
-import boto3
+from boto3 import Session
 
-session = boto3.session.Session()
+CREDENTIAL_REFRESH_INTERVAL_IN_SECONDS = int(os.getenv('CREDENTIAL_REFRESH_INTERVAL_IN_SECONDS', 55 * 60))
 
-creds = session.get_credentials()
-creds = creds.get_frozen_credentials()
 
-os.environ["AWS_ACCESS_KEY_ID"] = creds.access_key
-os.environ["AWS_SECRET_ACCESS_KEY"] = creds.secret_key
-os.environ["AWS_SESSION_TOKEN"] = creds.token
+def refresh_credentials(run_indefinitely=False):
+    session = Session()
+    credentials = session.get_credentials().get_frozen_credentials()
 
-os.system(" ".join(sys.argv[1:]))
+    while run_indefinitely:
+        set_credentials(credentials)
+        time.sleep(CREDENTIAL_REFRESH_INTERVAL_IN_SECONDS)
+
+    set_credentials(credentials)
+
+
+def set_credentials(frozen_credentials):
+    os.environ['AWS_ACCESS_KEY_ID'] = frozen_credentials.access_key
+    os.environ['AWS_SECRET_ACCESS_KEY'] = frozen_credentials.secret_key
+    os.environ['AWS_SESSION_TOKEN'] = frozen_credentials.token
+    print("Credentials set.")
+
+
+def command(cmd):
+    p = subprocess.Popen(
+        cmd
+    )
+    print(p.communicate())
+
+
+def main(cmd):
+    refresh_credentials(run_indefinitely=False)
+
+    credential_process = multiprocessing.Process(target=refresh_credentials, args=())
+    command_process = multiprocessing.Process(target=command, args=(cmd,))
+
+    credential_process.start()
+    command_process.start()
+
+    if not command_process.is_alive():
+        credential_process.terminate()
+
+    credential_process.join()
+    command_process.join()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
